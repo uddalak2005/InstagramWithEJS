b65f6c9c2941eb2641ff892c9384b998ff5c9be0
uddalak2005 <uddalakmukhopadhyay@gmail.com>
2024-11-26 09:20:58 +0530

    The backend and frontend and inst.ejs redering and account not found erroes are handled

--- server.js
+++ server.js
@@ -15,17 +15,20 @@ app.get('/', function(req, res) {
     res.render('home.ejs');
 })
 
-app.get('/id' , (req, res) => {
-    const { username } = req.query;
-    console.log(username)
-    res.send(username);
-})
-
-// app.get('/id/:username', (req, res) => {
-//     const { username } = req.params;
-//     console.log('Username:', username);
-//     res.send(`Received username: ${username}`);
-// });
+app.get('/id/:username', (req, res) => {
+    const { username } = req.params;
+    console.log('Username:', username);
+    const instaData = require('./data.json');
+    const userData = instaData[username];
+    if(userData){
+        console.log(userData);
+        res.render('insta.ejs', {userData: userData});
+    }
+    else{
+        res.render('404.ejs');
+    }
+    
+});
 
 app.listen(port, () => {
     console.log(`app is listening on port : ${port}`);
